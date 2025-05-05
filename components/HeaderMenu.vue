8965945cf3c1d43ff0281ca2ed73d2daf4462c18
Mihail Filimon <mih.fil76@gmail.com>
2025-05-05 21:00:52 +0300

    Maybe it was the nuxt booster

--- components/HeaderMenu.vue
+++ components/HeaderMenu.vue
@@ -4,9 +4,9 @@
 
 <template>
   <section class="flex flex-col lg:flex-row gap-10 xl:gap-15">
-    <ULink class="text-xl font-bold" to="/povesteaMea" raw active-class="font-bold px-3 border-l-4 lg:border-l-0 lg:border-b-4" inactive-class="text-(--ui-primary)" >Povestea mea</ULink>
-    <ULink class="text-xl font-bold" to="/servicii/" raw active-class="font-bold px-3 border-l-4 lg:border-l-0 lg:border-b-4" inactive-class="text-(--ui-primary)">Servicii</ULink>
-    <ULink class="text-xl font-bold" to="/blog/" raw active-class="font-bold px-3 border-l-4 lg:border-l-0 lg:border-b-4" inactive-class="text-(--ui-primary)">Blog</ULink>
+    <ULink class="text-xl font-bold" to="/povestea-mea" raw active-class="font-bold px-3 border-l-4 lg:border-l-0 lg:border-b-4" inactive-class="text-(--ui-primary)" >Povestea mea</ULink>
+    <ULink class="text-xl font-bold" to="/servicii" raw active-class="font-bold px-3 border-l-4 lg:border-l-0 lg:border-b-4" inactive-class="text-(--ui-primary)">Servicii</ULink>
+    <ULink class="text-xl font-bold" to="/blog" raw active-class="font-bold px-3 border-l-4 lg:border-l-0 lg:border-b-4" inactive-class="text-(--ui-primary)">Blog</ULink>
   </section>
 </template>
 
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,30 +7,34 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
     compatibilityDate: '2024-11-01',
     modules: [
-      '@nuxt/ui',
+      '@nuxtjs/seo',
       '@nuxt/content',
-      '@nuxt/icon',
+      '@nuxt/ui',
       '@nuxt/image',
-      '@nuxt/eslint',
-      '@nuxt/scripts',
       '@nuxtjs/color-mode',
-      '@nuxtjs/html-validator',
       '@primevue/nuxt-module',
       '@zadigetvoltaire/nuxt-gtm',
+      '@nuxt/icon',
+      '@nuxt/eslint',
+      '@nuxt/scripts',
+      '@nuxtjs/html-validator',
     ],
     app: {
         head: {
-            title: 'Auto Detail Iasi',
+            title: 'Bobby`s Auto Detailing Iasi',
             htmlAttrs: {
-                lang: 'en',
+                lang: 'ro',
             },
             link: [
-                {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
-                {rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png'},
-                {rel: 'icon', type: 'image/png', sizes: '96x96', href: '/favicon-96x96.png'},
-                {rel: 'manifest', href: '/site.webmanifest'},
+                { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+                { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+                { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/favicon-96x96.png' },
+                { rel: 'manifest', href: '/site.webmanifest' },
             ]
         },
+    },
+    nitro: {
+        compressPublicAssets: true,
     },
     devtools: {
         enabled: true,
@@ -39,12 +43,28 @@ export default defineNuxtConfig({
         },
     },
     css: ["~/assets/css/main.css"],
-    vite: {plugins: [tailwindcss(),]},
+    vite: { plugins: [tailwindcss(),] },
     components: true, // Ensure auto-import of components
     image: {
-        screens:{
-            icon:70,
-            icon2x:140
+        screens: {
+            icon: 70,
+            icon2x: 140,
+            default: 320,
+            xxs: 480,
+            xs: 576,
+            sm: 768,
+            md: 996,
+            lg: 1200,
+            xl: 1367,
+            xxl: 1600,
+            '4k': 1921
+        },
+
+        domains: ['img.youtube.com', 'i.vimeocdn.com'],
+
+        alias: {
+            youtube: 'https://img.youtube.com',
+            vimeo: 'https://i.vimeocdn.com',
         }
     },
     content: {
@@ -70,10 +90,10 @@ export default defineNuxtConfig({
         public: {
             gtm: {
                 id: 'GTM-K6SQMZ8S', // Replace with your GTM ID
-                compatibility:true,
+                compatibility: true,
                 enabled: true, // Set to false if you want to disable it in some environments
                 debug: false, // Set to true to enable console logs for debugging
-                loadScript:true,
+                loadScript: true,
                 enableRouterSync: true,
             },
         },
